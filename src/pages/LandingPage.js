da036cd034ba2fbe64353630dfc633b38fac589f
Paulo Jose Zapata <pauloz@Paulos-MacBook-Air.local>
2025-10-20 14:23:02 -0400

    Initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.css'; // Import the CSS for this page
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <div className="landing-content">
+        <h1 className="landing-title">College Notetakers</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
